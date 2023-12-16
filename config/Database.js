162b2517f187c24e4fbac49bcced8f01da7f6c6d
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-16 23:09:43 +0800

    update db

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import mysql2 from 'mysql2';
 //config
 const Database = new Sequelize('b2iobim6wv0db1b8vdtp','ualuujfll9mzkrde','R20jh72l4AxRrhCnuoxv',{
     host: "b2iobim6wv0db1b8vdtp-mysql.services.clever-cloud.com",
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: mysql2,
 });
 
 export default Database;
